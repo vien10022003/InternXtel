0d9e8b8bbb70dd1a3702ecfd5440facafceb6e9d
LENOVO-VIEN\vien1 <vien10.02.2003@gmail.com>
2026-05-25 17:15:48 +0700

    init project

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int[] arrrr= {4,3,2,1};        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println(Arrays.toString(arrrr));
+    }
+
+}
